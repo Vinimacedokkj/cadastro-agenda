6e4fd669cca8e931f89a7daf1abc06a9bf29872f
Vinicius Macedo <macedovinicius17@gmail.com>
2026-04-09 08:05:07 -0300

    adicionei a validação para as informações não ficarem vazias na criação do cadastro

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.getElementById("")
+const cadastrarUsuario = () => {
+
+    const validarInformacoesVazias = () => {
+        const novoUsuario = document.querySelector("#usuario").value.trim();
+        const novaSenha = document.querySelector("#senha-cadastro").value.trim();
+        const confirmarSenha = document.querySelector("#confirmar-senha-cadastro").value.trim();
+        
+        if (novoUsuario && novaSenha && confirmarSenha) {
+            if (novaSenha === confirmarSenha) {
+                alert(`Bem vindo, ${novoUsuario}`);
+
+            } else {
+                alert("As senhas não conferem");
+            }
+        } else {
+            alert("por favor, preencha todos os campos");
+        }
+    }
+
+    validarInformacoesVazias();
+}
+
+const botaoCadastrar = document.querySelector("#botao-cadastrar");
+botaoCadastrar.addEventListener("click", cadastrarUsuario);
