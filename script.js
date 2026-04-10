588e5986828cda819bf9a45503d87b5ab4737491
vinicius.macedo <macedovinicius17@gmail.com>
2026-04-09 23:42:15 -0300

    organizando a função principal de cadastrar usuarios

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 const cadastrarUsuario = () => {
+    const novoUsuario = document.querySelector("#usuario").value.trim();
+    const novaSenha = document.querySelector("#senha-cadastro").value.trim();
+    const confirmarSenha = document.querySelector("#confirmar-senha-cadastro").value.trim();
+    
+    if (novoUsuario && novaSenha && confirmarSenha) {
+        if (novaSenha === confirmarSenha) {
+            
 
-    const validarInformacoesVazias = () => {
-        const novoUsuario = document.querySelector("#usuario").value.trim();
-        const novaSenha = document.querySelector("#senha-cadastro").value.trim();
-        const confirmarSenha = document.querySelector("#confirmar-senha-cadastro").value.trim();
-        
-        if (novoUsuario && novaSenha && confirmarSenha) {
-            if (novaSenha === confirmarSenha) {
-                alert(`Bem vindo, ${novoUsuario}`);
-
-            } else {
-                alert("As senhas não conferem");
-            }
         } else {
-            alert("por favor, preencha todos os campos");
+            alert("As senhas não conferem");
         }
+    } else {
+        alert("por favor, preencha todos os campos");
     }
-
-    validarInformacoesVazias();
 }
 
 const botaoCadastrar = document.querySelector("#botao-cadastrar");
