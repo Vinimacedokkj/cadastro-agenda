a3bdd1fb3cc4cf766c0e4c7819c32b3a0c185df3
Vinicius Macedo <macedovinicius17@gmail.com>
2026-04-11 09:58:32 -0300

    adicionado a validação do usuario no cadastro

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+const verificarUsuariosCadastrados = localStorage.getItem("usuarios");
+
 const cadastrarUsuario = () => {
-    const novoUsuario = document.querySelector("#usuario").value.trim();
+    const novoUsuario = document.querySelector("#usuario-cadastro").value.trim();
     const novaSenha = document.querySelector("#senha-cadastro").value.trim();
     const confirmarSenha = document.querySelector("#confirmar-senha-cadastro").value.trim();
     
     if (novoUsuario && novaSenha && confirmarSenha) {
-        if (novaSenha === confirmarSenha) {
-            localStorage.setItem("usuario", "");
-                
+        if (novaSenha === confirmarSenha) {            
+            if (!verificarUsuariosCadastrados) {
+                const lista = [];
+                lista.push({"usuario": novoUsuario, "senha": novaSenha});
+                localStorage.setItem("usuarios", JSON.stringify(lista));
+                return [];
+            } else {
+                const pegarLista = JSON.parse(verificarUsuariosCadastrados);
+                pegarLista.push({"usuario": novoUsuario, "senha": novaSenha});
+                localStorage.setItem("usuarios", JSON.stringify(pegarLista));
+            }
         } else {
             alert("As senhas não conferem");
         }
@@ -17,3 +27,15 @@ const cadastrarUsuario = () => {
 
 const botaoCadastrar = document.querySelector("#botao-cadastrar");
 botaoCadastrar.addEventListener("click", cadastrarUsuario);
+
+const validarUsuario = () => {
+    // const usuario = document.querySelector("#usuario-login").value;
+    // const senha = document.querySelector("#senha-login").value
+
+    // console.log(verificarUsuariosCadastrados);
+
+    alert("hello world");
+}
+
+const botaoLogin = document.querySelector("#botao-login");
+botaoLogin.addEventListener("click", validarUsuario);
